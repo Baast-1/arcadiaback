e851848b643e24aad696828c45079c81f1c03592
Bastien <bastien.ter@gmail.com>
2024-11-08 14:26:48 +0100

    gestion des avis et installation de mongodb

---
+++ src/controllers/avisMongoController.js
@@ -0,0 +1,39 @@
+const AvisMongo = require('../models/avisMongo');
+
+exports.createAvis = async (req, res) => {
+    try {
+        const { pseudo, message } = req.body;
+        const newAvis = new AvisMongo({
+            pseudo,
+            message,
+            isVisible: false
+        });
+        await newAvis.save();
+        res.status(201).json(newAvis);
+    } catch (error) {
+        console.error('Erreur lors de la création de l\'avis:', error); // Affiche l'erreur
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.getAllAvis = async (req, res) => {
+    try {
+        const avis = await AvisMongo.find();
+        res.status(200).json(avis);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+exports.putAvis = async (req, res) => {
+    try {
+        const avis = await AvisMongo.findById(req.params.id);
+        if (!avis) {
+            return res.status(404).json({ error: 'Avis not found' });
+        }
+        await AvisMongo.findByIdAndUpdate(req.params.id, { ...req.body });
+        res.status(200).json({ message: 'Avis updated' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
