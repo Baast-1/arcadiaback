4d8d169ea4d495e6da0106fdf30ea0be22668d73
Bastien <bastien.ter@gmail.com>
2024-11-10 10:32:07 +0100

    update

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -10,6 +10,7 @@ const formData = require('form-data');
 const Mailgun = require('mailgun.js');
 const mailgun = new Mailgun(formData);
 const mg = mailgun.client({ username: 'api', key: process.env.MAILGUN_API_KEY });
+console.log("Mailgun API Key:", process.env.MAILGUN_API_KEY);
 
 function generatePassword(length) {
     const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
